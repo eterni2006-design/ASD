2b4efdb5434a2bbfe1dd79c62448535d329fd03e
timtum <eterny2006@gmail.com>
2025-12-03 12:31:46 +0300

    stack lib

--- tests/test_easy_example.cpp
+++ tests/test_easy_example.cpp
@@ -2,6 +2,8 @@
 
 #include <gtest/gtest.h>
 #include "../lib_easy_example/easy_example.h"
+#include "stack.h"
+#include <string>
 
 #define EPSILON 0.000001
 
@@ -48,3 +50,7 @@ TEST(TestEasyExampleLib, throw_when_try_div_by_zero) {
   // Act & Assert
   ASSERT_ANY_THROW(division(x, y));
 }
+
+
+
+
